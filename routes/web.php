ecdbdd69d046f370e4994fdaf3f2b8eb5abd9606
tomocito <tomocito@gmail.com>
2021-06-21 22:38:25 +0900

    authorごとの記事一覧表示

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     // すべての記事を取得
-    $posts = Post::with('category')->get();
+    $posts = Post::latest()->with('category')->get();
 
     // $posts（すべての記事のコレクション）をpostsとしてviewに送る
     return view('posts', [
@@ -27,5 +28,11 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 Route::get('/categories/{category:slug}', function (Category $category){
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author){
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
